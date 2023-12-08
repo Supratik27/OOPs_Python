742fa1fb24e0588a81f4b10b0248e38280719958
Supratik Sengupta <126981110+Supratik27@users.noreply.github.com>
2023-12-08 15:11:34 +0530

    Delete Getters & Setters directory

--- Getters & Setters/Polymorphism.py
+++
@@ -1,7 +0,0 @@
-name = "Supratik"  # str
-print(len(name))
-
-some_list = ["1", "name", "23"]  # list
-print(len(some_list))
-# That's polymorphism in action, a single function does now
-# how to handle different kinds of objects as expected!
